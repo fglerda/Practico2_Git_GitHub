468085dd36bfd13b907951dbb6aa0a39bfa8505a
flerda <fglerda@gmail.com>
2025-03-29 16:56:00 -0300

    se creo suma.py

---
+++ suma.py
@@ -0,0 +1,6 @@
+# calculadora de suma:
+num1 = int(input("Ingrese primer numero: "))
+num2 = int(input("Ingrese segundo numer: "))
+
+suma = num1 + num2
+print(f"La suma de {num1} y {num2} es = {suma}")
